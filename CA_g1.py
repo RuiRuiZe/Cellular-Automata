07c7934453129db652b0aa1bb6c29562ff6bff87
RuiRuiZe <1536564156@qq.com>
2025-11-17 13:37:38 +0800

    m

--- CA_g1.py
+++ CA_g1.py
@@ -4,7 +4,7 @@ from CA_anim import run_animation
 from CA_rules import ACTIVE, BORN, DEAD, IDLE, ACT1, ACT2
 
 # 状态定义
-rows, cols = 100, 100
+rows, cols = 200, 200
 
 def initialize_clusters(grid, num_clusters=5, cluster_size=3):
     for _ in range(num_clusters):
@@ -18,7 +18,7 @@ def initialize_clusters(grid, num_clusters=5, cluster_size=3):
                 if 0 <= nx < rows and 0 <= ny < cols:
                     # 随机转换为 A / B / D
                     grid[nx, ny] = np.random.choice([ACTIVE, BORN, DEAD],
-                                                    p=[0.9, 0.05, 0.05]) 
+                                                    p=[0.8, 0.15, 0.05]) 
                     # （可调）A：B：D = 6:3:1 的比例
 
     return grid
